ec646d10faf9b2fae08bb78ef9863d64c083c76e
ian <chiknrice@gmail.com>
2015-07-18 21:50:42 +1200

    automated deploy of schema to site [ci skip]

---
+++ push-schema.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+FILENAME="jen8583-$1.xsd"
+
+#echo "$FILENAME"
+
+cd target
+
+mkdir tmp
+
+cd tmp
+
+git clone https://github.com/chiknrice/chiknrice.github.io.git
+
+cp "../classes/jen8583.xsd" "chiknrice.github.io/schema/$FILENAME"
+
+cd chiknrice.github.io
+
+git add "schema/$FILENAME"
+
+git commit -m "added schema version $1"
+
+git push
