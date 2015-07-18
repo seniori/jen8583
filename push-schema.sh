86eab01181d4d56b1cd434243f57937e681fb1c7
ian <chiknrice@gmail.com>
2015-07-19 03:29:11 +1200

    fix push schema script

--- push-schema.sh
+++ push-schema.sh
@@ -4,22 +4,20 @@ FILENAME="jen8583-${1%-*}.xsd"
 
 echo "Pushing $FILENAME"
 
-exit 0
+cd target
 
-#cd target
-#
-#mkdir tmp
-#
-#cd tmp
-#
-#git clone https://github.com/chiknrice/chiknrice.github.io.git
-#
-#cp "../classes/jen8583.xsd" "chiknrice.github.io/schema/$FILENAME"
-#
-#cd chiknrice.github.io
-#
-#git add "schema/$FILENAME"
-#
-#git commit -m "added schema version $1"
-#
-#git push
+mkdir tmp
+
+cd tmp
+
+git clone https://github.com/chiknrice/chiknrice.github.io.git
+
+cp "../classes/jen8583.xsd" "chiknrice.github.io/schema/$FILENAME"
+
+cd chiknrice.github.io
+
+git add "schema/$FILENAME"
+
+git commit -m "added schema version $1"
+
+git push
