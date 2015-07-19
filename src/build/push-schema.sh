4dcca0fdb41aeab1942621ae8848d945e5eb901b
ian <chiknrice@gmail.com>
2015-07-19 19:46:09 +1200

    reverted mistakenly commented code

--- src/build/push-schema.sh
+++ src/build/push-schema.sh
@@ -4,20 +4,20 @@ FILENAME="jen8583-${1%-*}.xsd"
 
 echo "Pushing $FILENAME"
 
-#cd target
-#
-#mkdir tmp
-#
-#cd tmp
-#
-#git clone https://github.com/chiknrice/chiknrice.github.io.git
-#
-#cp "../classes/jen8583.xsd" "chiknrice.github.io/schema/$FILENAME"
-#
-#cd chiknrice.github.io
-#
-#git add "schema/$FILENAME"
-#
-#git commit -m "added schema version $1"
-#
-#git push
+cd target
+
+mkdir tmp
+
+cd tmp
+
+git clone https://github.com/chiknrice/chiknrice.github.io.git
+
+cp "../classes/jen8583.xsd" "chiknrice.github.io/schema/$FILENAME"
+
+cd chiknrice.github.io
+
+git add "schema/$FILENAME"
+
+git commit -m "added schema version $1"
+
+git push
